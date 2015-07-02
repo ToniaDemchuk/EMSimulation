455965c95f22b76e667bc00221489bd863b7e301
Tonia Kolkevych (work) <kami-sansan@mail.ru>
2015-07-02 18:20:57 +0300

    Fixed Stylecops in Gnuplot and ScilabEngine projects.

--- ScilabEngine/Engine/ScilabEngine.cs
+++ ScilabEngine/Engine/ScilabEngine.cs
@@ -3,8 +3,15 @@ using ScilabEngine.Helpers;
 
 namespace ScilabEngine.Engine
 {
+    /// <summary>
+    /// The ScilabEngine class.
+    /// </summary>
     public class ScilabEngine : IDisposable
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScilabEngine"/> class.
+        /// </summary>
+        /// <exception cref="System.Exception">Error while starting scilab engine</exception>
         public ScilabEngine()
         {
             if (ScilabEntryPoint.StartScilab(null, null, 0) != 1)
@@ -13,6 +20,11 @@ namespace ScilabEngine.Engine
             }
         }
 
+        /// <summary>
+        /// Executes the specified job.
+        /// </summary>
+        /// <param name="job">The job to execute.</param>
+        /// <exception cref="System.Exception">Error while executing script</exception>
         public void Execute(string job)
         {
             if (ScilabEntryPoint.SendScilabJob(job) != 0)
@@ -21,11 +33,19 @@ namespace ScilabEngine.Engine
             }
         }
 
+        /// <summary>
+        /// Executes the specified format.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <param name="objects">The objects.</param>
         public void Execute(string format, params object[] objects)
         {
             this.Execute(string.Format(format, objects));
         }
 
+        /// <summary>
+        /// Waits this instance.
+        /// </summary>
         public void Wait()
         {
             while (true)
@@ -36,6 +56,7 @@ namespace ScilabEngine.Engine
         /// <summary>
         /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
         /// </summary>
+        /// <exception cref="System.Exception">Error while closing scilab engine</exception>
         public void Dispose()
         {
             if (ScilabEntryPoint.TerminateScilab(null) != 1)
--- ScilabEngine/Helpers/ScilabHelper.cs
+++ ScilabEngine/Helpers/ScilabHelper.cs
@@ -1,9 +1,18 @@
 using System.Collections.Generic;
 
-namespace Simulation.DDA.Tests
+namespace ScilabEngine.Helpers
 {
+    /// <summary>
+    /// The ScilabHelper class.
+    /// </summary>
     public class ScilabHelper
     {
+        /// <summary>
+        /// Formats to array.
+        /// </summary>
+        /// <typeparam name="T">The type of enumerable.</typeparam>
+        /// <param name="dict">The dictionary.</param>
+        /// <returns>The string representing scilab array.</returns>
         public static string FormatToArray<T>(IEnumerable<T> dict)
         {
             var list = string.Join(",", dict);
