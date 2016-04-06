cf36c197679209e99c30506f276d628b3db9eea8
Tonia Kolkevych (work) <kami-sansan@mail.ru>
2016-04-06 17:54:36 +0300

    Added Voxel and MeshInfo to VoxelReader.cs. Modified FDTDProgram to read from voxel file.

---
+++ Simulation.Infrastructure/Models/MeshInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Infrastructure.Models
+{
+    public class MeshInfo
+    {
+        public List<Voxel> Voxels { get; set; }
+
+        public Voxel Resolution { get; set; }
+    }
+}
---
+++ Simulation.Infrastructure/Models/Voxel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Simulation.Infrastructure.Models
+{
+    public class Voxel
+    {
+        public int I { get; set; }
+        public int J { get; set; }
+        public int K { get; set; }
+    }
+}
