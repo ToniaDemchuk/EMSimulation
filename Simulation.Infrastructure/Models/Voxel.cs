bd190ecd411f0d7279287260d843e5a3fc7cb74d
Tonia Kolkevych (work) <kami-sansan@mail.ru>
2016-04-13 13:00:36 +0300

    Added ObjToVoxel reader.

--- Simulation.Infrastructure/Models/Voxel.cs
+++ Simulation.Infrastructure/Models/Voxel.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Security.Cryptography;
+
+using Simulation.Models.Coordinates;
+
 namespace Simulation.Infrastructure.Models
 {
     public class Voxel
     {
+        public Voxel()
+        {
+        }
+
+        public Voxel(CartesianCoordinate point)
+        {
+            this.I = (int) Math.Round(point.X);
+            this.J = (int) Math.Round(point.Y);
+            this.K = (int) Math.Round(point.Z);
+        }
+
         public int I { get; set; }
         public int J { get; set; }
         public int K { get; set; }
